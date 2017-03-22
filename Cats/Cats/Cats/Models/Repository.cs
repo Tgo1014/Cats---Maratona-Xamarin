207017a5f160bcd7690d7929345cbb78f14f1984
Tiago Araujo <taraujodesouza@gmail.com>
2017-03-21 21:42:49 -0300

    Initial commit

---
+++ Cats/Cats/Cats/Models/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cats.Model;
+
+namespace Cats.Models
+{
+    public class Repository
+    {
+        public async Task<List<Cat>> GetCats()
+        {
+            var Service = new Services.AzureService<Cat>();
+            var Items = await Service.GetTable();
+            return Items.ToList();
+        }
+
+
+        /* Versão JSON
+        public async Task<List<Cat>> GetCats()
+        {
+            List<Cat> Cats;
+            var URLWebAPI = "http://demos.ticapacitacion.com/cats";
+            using (var Client = new System.Net.Http.HttpClient())
+            {
+                var JSON = await Client.GetStringAsync(URLWebAPI);
+                Cats = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Cat>>(JSON);
+            }
+            return Cats;
+        }*/
+
+    }
+}
---
+++ Cats/Cats/Cats/ViewModels/CatsViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cats.Model;
+using Cats.Models;
+using Xamarin.Forms;
+
+namespace Cats.ViewModels
+{
+    public class CatsViewModel : INotifyPropertyChanged
+    {
+        private bool Busy;
+        public ObservableCollection<Cat> Cats { get; set; }
+        public Command GetCatsCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        
+        public CatsViewModel()
+        {
+            Cats = new ObservableCollection<Cat>();
+
+            GetCatsCommand = new Command(async () => await GetCats(), () => !IsBusy);
+        }
+
+        async Task GetCats()
+        {
+            if (!IsBusy)
+            {
+                Exception Error = null;
+                try
+                {
+                    IsBusy = true;
+                    var Repository = new Repository();
+                    var Items = await Repository.GetCats();
+
+                    Cats.Clear();
+                    foreach (var Cat in Items)
+                    {
+                        Cats.Add(Cat);
+                    }
+
+                } catch (Exception ex)
+                {
+                    Error = ex;
+                }
+                finally
+                {
+                    IsBusy = false;
+                    if (Error != null)
+                    {
+                        await Xamarin.Forms.Application.Current.MainPage.DisplayAlert("Error!", Error.Message, "OK");
+                    }
+                }
+
+            }
+            return;
+        }
+
+        public bool IsBusy
+        {
+            get
+            {
+                return Busy;
+            }
+            set
+            {
+                Busy = value;
+                OnPropertyChanged();
+                GetCatsCommand.ChangeCanExecute();
+            }
+        }
+
+    }
+}
